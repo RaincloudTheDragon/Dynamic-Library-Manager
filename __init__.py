c02d50b86f16d508499b3c5f7e118a8bf6df4cab
Raincloud <raincloudthedragon@gmail.com>
2025-08-20 17:04:08 -0600

    blender compiler init

--- __init__.py
+++ __init__.py
@@ -1,31 +1,17 @@
-bl_info = {
-    "name": "Dynamic Link Manager",
-    "author": "RaincloudTheDragon",
-    "version": (0, 5, 1),
-    "blender": (4, 5, 0),
-    "location": "View3D > Sidebar > Dynamic Link Manager",
-    "description": "Replace linked assets and characters with ease",
-    "warning": "",
-    "doc_url": "",
-    "category": "Import-Export",
-}
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import bpy
-from bpy.props import StringProperty, BoolProperty, EnumProperty
-from bpy.types import Panel, Operator, PropertyGroup
+def register(): ...
 
-# Import local modules
-from . import operators
-from . import ui
 
-# Registration
-def register():
-    operators.register()
-    ui.register()
-
-def unregister():
-    ui.unregister()
-    operators.unregister()
-
-if __name__ == "__main__":
-    register()
+def unregister(): ...
